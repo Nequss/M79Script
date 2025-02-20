94c0c8f1aae10be7d1a6b9787efbf4d9b43e3d8f
Nequss <snequs@gmail.com>
2025-02-20 12:03:24 +0100

    first commit

---
+++ script.pas
@@ -0,0 +1,163 @@
+// Configures server settings for M79 climbing mode
+//todo:
+// Checkpoints
+// ranking top10
+// my top
+// top3 cp times
+
+var
+  // Player positions (index 1-32 represents player ID)
+  PlayerX: array[1..32] of Single;  // X coordinates
+  PlayerY: array[1..32] of Single;  // Y coordinates
+  Timer: Array[1..32] of LongInt; // Timer for each player
+  Alive: array[1..32] of Boolean; // If players are alive
+
+// Returns the timer in the format "Minutes:Seconds:Miliseconds" Value = Ticks
+function ReturnTimer(Ticks: LongInt): String;
+var
+  TotalMilliseconds, Minutes, Seconds, Milliseconds: Integer;
+begin
+  // 60 ticks = 1000 milliseconds (1 second)
+  TotalMilliseconds := Ticks * (1000 div 60);
+
+  // Calculate minutes, seconds, and milliseconds
+  Minutes := TotalMilliseconds div 60000;
+  Seconds := (TotalMilliseconds mod 60000) div 1000;
+  Milliseconds := TotalMilliseconds mod 1000;
+
+  Result := IntToStr(Minutes) + ':' + IntToStr(Seconds) + ':' + IntToStr(Milliseconds);
+end;
+
+// Initialize the timer for Players
+procedure Ini;
+var i: integer;
+begin
+  for i := 1 to 32 do
+  begin
+    Timer[i] := 0;
+  end;
+end;
+
+procedure OnPlayerRespawn(ID: Byte);
+begin
+  Alive[ID] := true;
+  Timer[ID] := 0;
+end;
+
+procedure OnJoinTeam(ID, Team: byte);
+begin
+  Alive[ID] := true;
+end;  
+
+procedure OnPlayerKill(Killer, Victim: byte; Weapon: string);
+begin
+  Alive[Victim] := false;
+  Timer[Victim] := 0;
+end;
+
+// Main loop
+procedure AppOnIdle(Ticks: Integer);
+var S: integer;
+begin
+  AppOnIdleTimer := 1; // Default = 60 = 1 second
+
+  // Check every 5 minutes
+  if Ticks mod (3600 * 5) = 0 then
+  begin
+    WriteConsole(0, '!help for available commands!', $EE81FAA1);
+    WriteConsole(0, 'Current Map: ' + CurrentMap + ' Next Map: ' + NextMap, $EE81FAA1);
+  end;
+
+  // Check every 1 s
+  if Ticks mod 60 = 0 then
+  begin
+    // WriteConsole(0, '1 second passed', $EE81FAA1);
+    // More code here
+  end;
+
+  for S := 1 to 32 do
+  begin
+    // Check if player is alive and if so calculate and draw timer
+    if Alive[S] = true then
+    begin
+      Timer[S] := Timer[S] + 1;
+      DrawText(S, 'Timer: ' + ReturnTimer(Timer[S]), 100, RGB(255,255,255), 0.10, 1, 100);
+    end;
+
+    // Check if player has no primary weapon
+    if GetPlayerStat(S, 'Primary') = 255 then
+    begin
+      ForceWeaponEx(S, 11, 11, 1, 1); // Give primary weapon Flamer
+    end;
+
+    if GetKeyPress(S, 'Reload') = true then DoDamage(S, 4000); // Damage player on R key press
+    if GetPlayerStat(S, 'Grenades') < 2 then GiveBonus(S, 4); // Give grenades
+  end;
+end;
+
+// Executes when a player joins the game
+procedure OnJoinGame(ID: Byte; Team: Byte);
+begin
+  WriteConsole(0, 'Welcome to the [Freestyle] M79 Climb ' + IDToName(ID) + '! Use !help for available commands.', $EE81FAA1);
+end;
+
+ procedure SendHighscore(ID: byte; Map: string; Time: LongInt);
+ var json: string;
+ begin
+  json := '{"Ip": "' + IDToIP(ID) + '", "Name": "' + IDToName(ID) + '", "Map": "' + Map + '", "Time": ' + IntToStr(Time) + '}';
+  WriteFile('highscore.json', json);
+ end;
+
+ procedure OnFlagScore(ID: Byte; TeamFlag: byte);
+ begin
+  WriteConsole(0, IDToName(ID) + ' finished the map in ' + ReturnTimer(Timer[ID]), $EE81FAA1);
+  SendHighscore(ID, CurrentMap, Timer[ID]);
+  DoDamage(ID, 4000);
+ end;
+
+
+ procedure DisplayTop(Map: string);
+ begin
+  // to do
+ end;
+
+ procedure DisplayMyTop(ID: byte; Map: string);
+ begin
+
+ end;
+
+// Executes when a player speaks
+procedure OnPlayerSpeak(ID: Byte; Text: string);
+begin
+  case lowercase(getpiece(Text, ' ', 0)) of 
+    // '!quit': Shutdown;
+    '!next', '!nextmap', '!nm', '!v': Command('/nextmap');
+    '!top': begin
+              DisplayTop(CurrentMap);
+            end;
+    '!mytop': begin
+                DisplayMyTop(ID, CurrentMap);
+              end;
+    '!save': begin
+                PlayerX[ID] := GetPlayerStat(ID, 'X');
+                PlayerY[ID] := GetPlayerStat(ID, 'Y');
+             end;
+    '!load': begin
+                MovePlayer(ID, PlayerX[ID], PlayerY[ID]);
+             end;  
+    '!help': begin
+               WriteConsole(0, 'Available commands: !save, !load, !next, !nextmap, !nm', $EE81FAA1);
+             end; 
+    '!top', '!top10', '!best', '!times': begin
+                                            DisplayTop10(CurrentMap);
+                                         end; 
+    '!mytop', '!mybest', '!mytimes': begin
+                                       DisplayMyTop10(ID, CurrentMap);
+                                     end;
+  end;
+end;
+
+// Initialize the game mode settings
+begin
+  Ini;
+end.
